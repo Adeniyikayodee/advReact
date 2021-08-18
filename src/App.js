4acde57a2c24b6d37cf5f8825334dba47d1778b7
Kayode Adeniyi <adeniyikayode22@gmail.com>
2021-08-18 12:13:31 +0100

    fetched data with hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Movie from './hoc/movie';
-import Counter from './hooks/Counter'
+import Counter from './hooks/Counter';
+import Users from './hooks/Users'
 
 function App() {
   return (
-    <Counter />
+    <Users />
   );
 }
 
